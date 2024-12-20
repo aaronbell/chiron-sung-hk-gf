072d6f1499a95aed52792092d7f300edfc3f3878
Aaron <aaron@sajatypeworks.com>
2024-12-20 08:48:39 -0800

    Remove prep table

--- source/post.py
+++ source/post.py
@@ -6,5 +6,9 @@ for file in Path("fonts/variable").glob("*.ttf"):
 	font = TTFont(str(file))
 	if font["gasp"].gaspRange != {65535: 0x000A}:
 		font["gasp"].gaspRange = {65535: 0x000A}
+	try:
+		del font["prep"]
+	except KeyError:
+		pass
 
 	font.save(file)
